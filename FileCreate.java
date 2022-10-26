9652231b9c09ed62f3c5e6c6df4cd68c8b050540
Manikandan <89195312+manikandan531@users.noreply.github.com>
2022-10-26 19:16:43 +0530

    Update FileCreate.java

--- FileCreate.java
+++ FileCreate.java
@@ -1,5 +1,20 @@
 package filehandling;
 
-public class FileCreate {
-
+public class FileCreate
+{
+  public static void main(String[] args)
+	   {
+	       File folder=new File("D:\\Trainning\\FileHandling\\Practice\\SubFolder\\test.xls");
+	        boolean present=folder.exists();
+	        if(present ==false)
+	        try 
+	       {
+	        	boolean created =folder.createNewFile();
+	        	System.out.println("File created "+ created);
+	        }
+        catch(IOException v)
+        	{
+        	System.out.println("error occured");
+	        }
+	    }
 }
