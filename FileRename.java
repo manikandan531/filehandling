72ca7b9e44a2f1031a3bc849ec9d54ce233e31b3
Manikandan <89195312+manikandan531@users.noreply.github.com>
2022-10-27 14:06:22 +0530

    Update FileRename.java

--- FileRename.java
+++ FileRename.java
@@ -4,9 +4,9 @@ public class FileRename
 {
   public static void main(String[] args)
 	   {    
-	    File folder=new File("D:\\Training\\FileHandling\\Practice\\SubFolder\\test.xls");
+	    File folder=new File("D:\\Training\\FileHandling\\Practice\\SubFolder\\test.xlsx");
 	                    
-	     File newName=new File("D:\\Training\\FileHandling\\Practice\\SubFolder\\Renamed.xls");
+	     File newName=new File("D:\\Training\\FileHandling\\Practice\\SubFolder\\Renamed.xlsx");
 	      boolean renameSuccess=folder.renameTo(newName);
 	      System.out.println("renameSuccess "+ newName);
 	    }
